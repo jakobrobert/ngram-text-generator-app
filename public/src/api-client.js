74d2b042f7f3c1e47b549db82d4dce3c24e2de1d
Jack-42 <jakob-robert@hotmail.de>
2020-11-05 16:45:49 +0100

    Extract API calls into new class APIClient

---
+++ public/src/api-client.js
@@ -0,0 +1,39 @@
+class APIClient {
+    constructor(baseURL) {
+        this.baseURL = baseURL;
+        this.model = undefined;
+    }
+
+    async buildModel(order, trainingText) {
+        const url = this.baseURL + "/build-model";
+        const data = {
+            order: order,
+            training_text: trainingText
+        };
+        return await this.postData(url, data);
+    }
+
+    async generateText(startText, textLength, model) {
+        const url = this.baseURL + "/generate-text";
+        const data = {
+            start_text: startText,
+            text_length: textLength,
+            model: model
+        };
+        return await this.postData(url, data);
+    }
+
+    async postData(url, data) {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+        if (!response.ok) {
+            throw new Error("Request failed: " +  response.statusText);
+        }
+        return response.json();
+    }
+}
--- public/src/api-test.js
+++ public/src/api-test.js
@@ -1,62 +1,46 @@
-// TODO: Only test code, integrate and remove later
-const BASE_URL = "https://jack0042.uber.space/ngram-text-generator-api";
+// TODO: Only test code, remove later
 
 async function runAPITest() {
-    await testBuildModel();
-    await testGenerateText();
+    const api = new APIClient("https://jack0042.uber.space/ngram-text-generator-api");
+    await testBuildModel(api);
+    await testGenerateText(api);
 }
 
-async function testBuildModel() {
-    const url = BASE_URL + "/build-model";
-    const data = {
+async function testBuildModel(api) {
+    console.log("testBuildModel");
+
+    const order = 3;
+    const trainingText = "Hello, what is going on?";
+
+    const response = await api.buildModel(order, trainingText);
+    console.log(response);
+}
+
+async function testGenerateText(api) {
+    console.log("testGenerateText");
+
+    const startText = "All I";
+    const textLength = 100;
+    const model = {
         order: 3,
-        training_text: "Hello, what is going on?"
-    };
-    const response = await postData(url, data);
-    console.log("test build-model");
-    console.log(response);
-}
-
-async function testGenerateText() {
-    const url = BASE_URL + "/generate-text";
-    const data = {
-        start_text: "All I",
-        text_length: "100",
-        model: {
-            order: 3,
             ngrams: [{
-                history: [0, 1],
-                predictions: [{
-                    token: 2,
-                    probability: 0.1111111111111111
-                }, {
-                    token: 5,
-                    probability: 0.8888888888888888
-                }]
-            }, {
-                history: [1, 2],
-                predictions: [{
-                    token: 3,
-                    probability: 1
-                }]
+        history: [0, 1],
+        predictions: [{
+            token: 2,
+            probability: 0.1111111111111111
+        }, {
+            token: 5,
+            probability: 0.8888888888888888
+        }]
+        }, {
+            history: [1, 2],
+            predictions: [{
+                token: 3,
+                probability: 1
             }]
-        }
+        }]
     };
-    const response = await postData(url, data);
-    console.log("test generate-text");
-    console.log(response);
-}
 
-async function postData(url, data) {
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    });
-    if (!response.ok) {
-        throw new Error("Request failed: " +  response.statusText);
-    }
-    return response.json();
+    const response = await api.generateText(startText, textLength, model);
+    console.log(response);
 }
